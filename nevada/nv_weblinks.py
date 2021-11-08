a838d5d0bcecaf9fede119c222dc7927cb49af05
Katherine Chang <kachang@uw.edu>
2021-11-08 01:02:54 -0800

    Added NV scrape func and unit test

---
+++ nevada/nv_weblinks.py
@@ -0,0 +1,62 @@
+"""Weblinks for Nevada committee meeting pages, organized by committee name (2011-2021)"""
+
+comlabor_links=[
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/340/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/339/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/219/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/215/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/184/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/187/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/47/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/68/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/1/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/10/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/24/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/33/Meetings"
+]
+
+ed_links=[
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/342/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/348/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/228/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/216/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/168/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/185/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/50/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/66/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/2/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/13/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/25/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/36/Meetings"
+]
+
+hhs_links=[
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/346/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/350/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/232/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/221/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/170/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/170/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/52/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/63/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/5/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/22/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/28/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/45/Meetings"
+    ]
+
+nram_links=[
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/349/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/343/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/225/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/235/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/186/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/174/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/67/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/56/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/16/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/7/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/39/Meetings",
+    "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/30/Meetings"
+]
+
