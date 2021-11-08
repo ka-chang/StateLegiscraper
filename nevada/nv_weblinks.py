8f571e7df8ba1238aa123377fcffbb3b29c04e19
YuxinSIA <xin1116@uw.edu>
2021-11-08 15:36:32 -0800

    Update nv_weblinks.py
    Added committee web links for Assembly and Senate: Government Affairs, Growth and Infrastructure, Judiciary, Revenue and Economic Development, Ways and Means/Finance.

--- nevada/nv_weblinks.py
+++ nevada/nv_weblinks.py
@@ -39,6 +39,51 @@ assem_nram=[
     "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/30/Meetings"
 ]
 
+assem_ga=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/344/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/230/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/179/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/62/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/4/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/27/Meetings"
+]
+
+assem_gri=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/345/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/239/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/169/Meetings", # "Transportation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/51/Meetings", # "Transportation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/8/Meetings", # "Transportation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/31/Meetings" # "Transportation"
+] 
+
+assem_jud=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/335/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/233/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/188/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/69/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/6/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/29/Meetings"
+]
+
+assem_rev=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/347/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/236/Meetings", # "Taxation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/177/Meetings", # "Taxation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/60/Meetings", # "Taxation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/19/Meetings", # "Taxation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/42/Meetings"
+]
+
+assem_wm=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/337/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/238/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/178/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/61/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/9/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/32/Meetings"
+]
+
 #SENATE
 
 sen_comlabor=[
@@ -77,4 +122,48 @@ sen_nram=[
     "https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/39/Meetings"
 ]
 
+sen_ga=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/334/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/220/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/183/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/65/Overview",
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/12/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/35/Meetings"
+]
 
+sen_gri=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/338/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/231/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/182/Meetings", # "Transportation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/64/Meetings", # "Transportation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/18/Meetings", # "Transportation"
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/41/Meetings" # "Transportation"
+] 
+
+sen_jud=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/329/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/222/Overview",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/173/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/55/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/14/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/37/Meetings"
+]
+
+
+sen_red=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/331/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/226/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/171/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/53/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/17/Overview",
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/40/Meetings"
+]
+
+sen_fin=[
+	"https://www.leg.state.nv.us/App/NELIS/REL/81st2021/Committee/332/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/80th2019/Committee/218/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/79th2017/Committee/166/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/78th2015/Committee/48/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/77th2013/Committee/11/Meetings",
+	"https://www.leg.state.nv.us/App/NELIS/REL/76th2011/Committee/34/Meetings"
+]
