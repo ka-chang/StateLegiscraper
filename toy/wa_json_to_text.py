41e3defa73dd5eead1a791447759e5a6ee172a74
Katherine Chang <kachang@uw.edu>
2021-11-10 23:05:11 -0800

    Cleaned up folders and hidden files

--- toy/wa_json_to_text.py
+++ toy/wa_json_to_text.py
@@ -8,8 +8,8 @@ data = json.load(file)
 data_trim = data["results"]
 transcript=""
 
-for num in range(len(data_trim)):
-    transcript+=str(data_trim[num]["alternatives"][0]["transcript"])
+for i in range(len(data_trim)):
+    transcript+=str(data_trim[i]["alternatives"][0]["transcript"])
 
 transcript_txt = open('/Users/katherinechang/Google Drive/My Drive/2021/Fall 2021/CSE583/project/leg-text-scraper/toy/data_txt/wa_senate_bfst_2021_0119_GOOG.txt', 'w')
 transcript_txt.write(transcript)
