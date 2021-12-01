1bead140f120ed3489c0bb9f2f08fa72a745bdcb
Katherine Chang <kachang@uw.edu>
2021-11-30 21:35:53 -0800

    Updated docustrings for WA and NV

--- LegTextScraper/states/wa.py
+++ LegTextScraper/states/wa.py
@@ -1,54 +1,67 @@
+"""
+WA module for scrapping and processing text from https://leg.wa.gov 
+
+# Status
+
+Current Coverage:
+    [X] Committee Hearings (Audio Links) (2015 - 2020)
+
+Planned Coverage:
+    [ ] Committee Hearings (Video Links) (2000 - 2014)
+    [ ] Floor Speeches (Video Links)
+
+# WA Work Flow
+
+CLASS WAScrape
+
+    - wa_scrape_meeting_links by desired committee and legislative session. 
+    Function searches TVW archives for links to each individual committee meeting for that leg session
+    
+    - wa_scrape_audio by wa_meeting_links output
+    Function visits each link individually and gather weblinks for each meeting hearing audio by mp3 link on page
+    
+    -wa_scrape_download_audio by wa_scrape_audio output
+    Function gives the user option to download all mp3 locally
+    Pulls input by wa_scrape_audio for the weblinks by desired committee and leg session
+    Rename the file names by committee name and date (YYYYMMDD) (e.g. wa_education_20200305.mp3)
+
+CLASS WAProcess
+
+    - wa_speech_to_text
+    Function gives the user option to convert audio file to a text transcript through DeepSpeech
+    Uses mp3 links directly to process the transcripts
+    Downloads the transcript in json form, single json for each committee/legislative session
+    
+    - wa_text_clean
+    Function conducts tests and run light cleaning to ensure transcript is ready for text analysis
+
+"""
+
 import os
 from pathlib import Path
 import sys
 import re
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 
-"""
--------
-WA Work Flow
--------
+#from selenium.webdriver.common.action_chains import ActionChains
+#from selenium.webdriver.support import expected_conditions as EC
 
-CLASS wa_scrape
 
-1. wa_scrape_meeting_links by desired committee and legislative session. 
-Function will search TVW archives for links to each individual committee meeting for that leg session
-
-2. wa_scrape_audio with wa_meeting_links input
-Function will visit each link individually and gather weblinks for each meeting hearing audio by mp3 link on page
-
-3. wa_scrape_download_audio with wa_scrape_audio function
-Function will give the user option to download all mp3 locally
-Pulls input by wa_scrape_audio for the weblinks by desired committee and leg session
-Rename the file names by committee name and date (YYYYMMDD) (e.g. wa_education_20200305.mp3)
-
-CLASS speech_to_text
-
-4. wa_speech_to_text
-Function will give the user option to convert audio file to a text transcript through DeepSpeech
-Uses mp3 links directly to process the transcripts
-Downloads the transcript in json form, single json for each committee/legislative session
-
-5. wa_preprocess_transcript
-Function will conduct tests and run light cleaning to ensure transcript is ready for nltk text analysis
-
-"""
 
 def wa_meeting_links():
 
   """
   """
   
-# DRIVE SETUP
-service = Service("/Users/katherinechang/Google Drive/My Drive/2021/Fall 2021/CSE583/project/chromedriver")
+# DRIVER SETUP
+service = Service("/Users/katherinechang/Google Drive/My Drive/2021/Fall 2021/CSE583/project/chromedriver_64")
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
@@ -57,28 +70,85 @@ driver.get("https://www.tvw.org/archives/")
 
 # ENTER SPECIFIC COMMITTEE NAME AND PRESS ENTER 
 
+"""
 input_search = driver.find_element(By.ID, "invintus-archives-search")
 input_search.send_keys("House Education Committee")
 input_search.send_keys(Keys.RETURN)
+"""
 
 # SELECT CATEGORY LEGISLATIVE
 
-driver.find_element(By.CLASS_NAME, "selectric-invintus-archives-filter-category").click()
+#driver.find_element(By.CLASS_NAME, "selectric-invintus-archives-filter-category").click()
 #driver.find_element(By.CLASS_NAME, "selectric-hide-select").click()
 
 dropdown_category=driver.find_element(By.CLASS_NAME, "invintus-archives-filter-category")
 
 dropdown_category_select=Select(dropdown_category)
 
-dropdown_category_select.select_by_index(2).click()
+dropdown_category_select.select_by_index(2)
 
+dropdown_category_select.select_by_visible_("Well Read")
+
+
+selectric_option=driver.find_element(By.CLASS_NAME, "selectric")
+print(selectric_option)
+
+selectric_option.get_attribute('innerHTML')
+
+selectric_option.get_attribute('innerHTML') 
+
+# write script
+script =  '<p class="label">Legislative</p><b class="button">▾</b>'
+  
+# generate a alert via javascript
+selectric_option.execute_script(script)
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import time
+
+driver = webdriver.Chrome()
+action = ActionChains(driver)
+
+driver.get("URL")
+time.sleep(5)
+
+Para_Element = driver.find_element_by_id("myP")
+print(selectric_option.text)
+# driver.execute_script("var ele=arguments[0]; ele.innerHTML = 'Successfully Changed the Content';", Para_Element)
+
+driver.execute_script(script)
+element = driver.find_element_by_tag_name("a")
+driver.execute_script("arguments[0].setAttribute('href', 'https://www.google.com/')", element)
+driver.execute_script("var ele=arguments[0]; ele.innerHTML = 'Google';", element)
+
+Para_Element = driver.find_element_by_id("myP")
+print(Para_Element.text)
+
+
+####
+
+options = [x for x in selectric_option.find_elements_by_tag_name("option")]
+
+for element in options:
+    print(element.get_attribute("value"))
+    
+dropdown_category.select_by_visible_text("Well Read")
+    
+options
+    
+.click()
+
+print(dropdown_category_select)
 
 #####
 
 
 options = driver.find_elements(By.TAG_NAME, "option")
 
-for each_option in options:
+for each_option in dropdown_category:
     #print(each_option)
     print(each_option.get_attribute("value"))
     
