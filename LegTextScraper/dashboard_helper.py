3443607d79e65ba4d9e200a142f8e818d905874b
shujiechen11 <91713577+shujiechen11@users.noreply.github.com>
2021-12-04 19:51:19 -0800

    update the py file because of indent errors

--- LegTextScraper/dashboard_helper.py
+++ LegTextScraper/dashboard_helper.py
@@ -16,7 +16,7 @@ import torch
 
 class NVHelper:
     
-   def nv_extract_month(nv_json_path):
+    def nv_extract_month(nv_json_path):
        """
        
        Parameters
@@ -67,50 +67,52 @@ class NVHelper:
        return(new_json_file)
 
     def filter_covid_sentences(data_by_date):
-    """
+        """
 
-    Parameters
-    ----------
-    data_by_date json file 
-    Returns
-    -------
-    """
-    list_sen=[]
-    k = 0
-    for i in data_by_date.keys():
-        temp = nltk.tokenize.sent_tokenize(data_by_date[i])
-        for j in range(len(temp)):
-            list_sen.append({'text':temp[j], 'metadata': i.strftime("%m/%d/%Y")+"-"+str(j)})
-            k += 1
-    corpus = [text['text'] for text in list_sen]
-    embedder = SentenceTransformer('all-MiniLM-L6-v2')
-    corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
-    query = 'COVID 19'
-    top_k = len(corpus)//20
+        Parameters
+        ----------
+        data_by_date json file 
+        Returns
+        -------
+        """
+        list_sen=[]
+        k = 0
+        for i in data_by_date.keys():
+            temp = nltk.tokenize.sent_tokenize(data_by_date[i])
+            for j in range(len(temp)):
+                list_sen.append({'text':temp[j], 'metadata': i.strftime("%m/%d/%Y")+"-"+str(j)})
+                k += 1
+        corpus = [text['text'] for text in list_sen]
+        embedder = SentenceTransformer('all-MiniLM-L6-v2')
+        corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+        query = 'COVID 19'
+        top_k = len(corpus)//20
 
-    results = set()
-    query_embedding = embedder.encode(query, convert_to_tensor=True)
-    cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
-    top_results = torch.topk(cos_scores, k=top_k)
-    for score, idx in zip(top_results[0], top_results[1]):
-        if len(corpus[idx]) > 10:
-            results.add(corpus[idx])
-    filter = {}
-    for i in data_by_date.keys():
-        temp=nltk.tokenize.sent_tokenize(data_by_date[i])
-        filter[i] = []
-        for j in range(len(temp)):
-            if temp[j] in results:
-                filter[i].append(temp[j])
-            else:
-                pass
-    filter_m = defaultdict(list)
-    for key in filter.keys():
-        temp = filter[key]
-        month = key.month
-        filter_m[month].append(temp) # filtered sentences by month
-    for i in filter_m.keys():
-        filter_m[i]=sum(filter_m[i], [])
+        results = set()
+        query_embedding = embedder.encode(query, convert_to_tensor=True)
+        cos_scores = util.pytorch_cos_sim(query_embedding, corpus_embeddings)[0]
+        top_results = torch.topk(cos_scores, k=top_k)
+        for score, idx in zip(top_results[0], top_results[1]):
+            if len(corpus[idx]) > 10:
+                results.add(corpus[idx])
+        filter = {}
+        for i in data_by_date.keys():
+            temp=nltk.tokenize.sent_tokenize(data_by_date[i])
+            filter[i] = []
+            for j in range(len(temp)):
+                if temp[j] in results:
+                    filter[i].append(temp[j])
+                else:
+                    pass
+        filter_m = defaultdict(list)
+        for key in filter.keys():
+            temp = filter[key]
+            month = key.month
+            filter_m[month].append(temp) # filtered sentences by month
+        for i in filter_m.keys():
+            filter_m[i]=sum(filter_m[i], [])
 
-    with open("filtered_sentences_hhs.json", 'w') as f: 
-        json.dump(filter_m, f, ensure_ascii=False)
+        with open("filtered_sentences_hhs.json", 'w') as f: 
+            json.dump(filter_m, f, ensure_ascii=False)
+
+
